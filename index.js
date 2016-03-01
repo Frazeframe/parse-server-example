74d7df711011f49983297136327a8980f2663a00
Lukas Carvajal <lukascarvajal@gmail.com>
2016-02-29 23:08:52 -0600

    Remove production push notification logic

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ var api = new ParseServer({
         pfx: process.env.DEV_PUSH_CERTIFICATE_PATH || 'FFDevPushCert.p12',
         bundleId: 'appsByLukas.com.Fraze-Frame',
         production: false
-      }, 
-      {
-        pfx: process.env.PROD_PUSH_CERTIFICATE_PATH || 'FFProdPushCert.p12',
-        bundleId: 'appsByLukas.com.Fraze-Frame',  
-        production: true 
       }
     ]
   }
