4157b2a7e545ae71dc62b3ff62467355ee1b2a2e
Lukas Carvajal <lukascarvajal@gmail.com>
2016-04-24 16:11:32 -0500

    Add cloud code method for voting

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,18 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('voteOnPhoto', function(request, response) {
+	var query = new Parse.Query("Vote");
+  query.equalTo("createdBy", request.params.user);
+  query.equalTo("photo", request.params.photo);
+
+  query.find({
+    success: function(results) {
+      response.success(results);
+    },
+    error: function() {
+      response.error("Vote lookup failed");
+    }
+  });
+});
