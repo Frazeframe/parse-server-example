aea8c0a380b5531f2e0e3b40fc93a1cdaa24cc3f
Lukas Carvajal <lukascarvajal@gmail.com>
2016-04-24 22:41:04 -0500

    Add code for incrementing new vote weights

--- cloud/main.js
+++ cloud/main.js
@@ -1,77 +1,20 @@
-Parse.Cloud.useMasterKey();
+Parse.Cloud.afterSave('Vote', function(request) {
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
+	var UserClass = Parse.Object.extend("User");
+	var query = new Parse.Query(UserClass);
+	query.equalTo("objectId", request.object.get("createdBy").id);
 
-Parse.Cloud.define('voteOnPhoto', function(request, response) {
-	 Parse.Cloud.useMasterKey();
-
-	var query = new Parse.Query("Vote");
-  query.equalTo("createdBy", request.params.createdBy);
-  query.equalTo("photo", request.params.photo);
-
-  query.find({
-    success: function(results) {
-    	if (results[0] != null) {
-    		response.error("An error occured when searching for the vote.");
-    		var PhotoClass = Parse.Object.extend("Photo");
-				var photoQuery = new Parse.Query(PhotoClass);
-				photoQuery.get(request.params.photo, {
-				  success: function(photo) {
-				    // The object was retrieved successfully.
-				  },
-				  error: function(object, error) {
-				    // The object was not retrieved successfully.
-				    // error is a Parse.Error with an error code and message.
-				  }
-				});
-
-    		// var vote = results[0];
-
-
-    		// response.success(vote);
-
-    	}
-    	else {
-    		response.error("An error occured when searching for the vote.");
-    	}
-    },
-    error: function() {
-    	voteWeight = request.params.voteWeight
-    	photoId = request.params.photo
-
-    	if (voteWeight == 1 || voteWeight == -1) {
-    		var PhotoClass = Parse.Object.extend("Photo");
-				var photoQuery = new Parse.Query(PhotoClass);
-				photoQuery.get(photoId, {
-				  success: function(photo) {
-				  	photo.increment("totalVotes", voteWeight);
-				  	photo.save();
-
-				    var VoteClass = Parse.Object.extend("Vote");
-						var newVote = new VoteClass();
-
-						newVote.save({
-						  weight: request.params.voteWeight,
-						  photo: photo
-						}, {
-						  success: function(newVote) {
-						    response.success(newVote);
-						  },
-						  error: function(newVote, error) {
-						    response.error("There was an error saving the vote.");
-						  }
-						});
-				  },
-				  error: function(object, error) {
-				    response.error("An error occured when searching for the photo.");
-				  }
-				});
-    	}
-    	else {
-    		response.error("Invalid vote weight. Must be 1 for up vote or -1 for down vote.");
-    	}
-    }
-  });
-});
+	Parse.Push.send({
+	  where: query,
+	  data: {
+	    alert: "You got a push notification!"
+	  }
+	}, {
+	  success: function() {
+	    // Push was successful
+	  },
+	  error: function(error) {
+	    // Handle error
+	  }
+	});
+})
