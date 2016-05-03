386ad2023febf31242394c6bb0e128971aa0d57c
Lukas Carvajal <lukascarvajal@gmail.com>
2016-05-02 22:40:07 -0500

    Send push notification to user whose photo gets voted on

--- cloud/main.js
+++ cloud/main.js
@@ -2,33 +2,46 @@ Parse.Cloud.afterSave('Vote', function(request) {
 
 	var voteWeight = request.object.get("weight");
 
-	var photo = request.object.get("photo");
+	var photoPointer = request.object.get("photo");
 
-	var user = photo.object.get("createdBy");
-	var userId = user.id;
+	console.log(photoPointer.id)
 
-	if (voteWeight == 1) {
-		var targetUser = new Parse.User();
-		targetUser.id = userId;
+	var PhotoClass = Parse.Object.extend("Photo");
+	var query = new Parse.Query(PhotoClass);
+	query.get(photoPointer.id, {
+	  success: function(photo) {
+	    var user = photo.get("createdBy");
+			console.log(user)
 
-		var query = new Parse.Query(Parse.Installation);
-		query.equalTo('user', user);
+			var userId = user.id;
 
-		Parse.Push.send({
-		  where: query,
-		  data: {
-		    alert: 'You got an upvote on your photo!',
-		    badge: 'Increment',
-		    sound: 'default'
-		  }
-		}, {
-		  useMasterKey: true,
-		  success: function() {
-		    console.log("Notification for vote sent!");
-		  },
-		  error: function(error) {
-		    console.log(error);
-		  }
-		});
-	}
+			if (voteWeight == 1) {
+				var targetUser = new Parse.User();
+				targetUser.id = userId;
+
+				var query = new Parse.Query(Parse.Installation);
+				query.equalTo('user', user);
+
+				Parse.Push.send({
+				  where: query,
+				  data: {
+				    alert: 'You got an upvote on your photo!',
+				    badge: 'Increment',
+				    sound: 'default'
+				  }
+				}, {
+				  useMasterKey: true,
+				  success: function() {
+				    console.log("Notification for vote sent!");
+				  },
+				  error: function(error) {
+				    console.log(error);
+				  }
+				});
+			}
+	  },
+	  error: function(object, error) {
+	    console.log("Error getting photo object.")
+	  }
+	});
 });
