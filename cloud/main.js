5c7ade40ba9163eedffd5660c99d1f025bc44669
Lukas Carvajal <lukascarvajal@gmail.com>
2016-05-21 15:49:43 -0500

    Update push notifications to use username in notification

--- cloud/main.js
+++ cloud/main.js
@@ -1,40 +1,61 @@
+//-----------------------------------------------------------------------------
+// Helper methods
+//-----------------------------------------------------------------------------
+
+function sendPushNotificationMessageWithUser(message, user) {
+  var query = new Parse.Query(Parse.Installation);
+	query.equalTo('user', user);
+
+	Parse.Push.send({
+	  where: query,
+	  data: {
+	    alert: message,
+	    badge: 'Increment',
+	    sound: 'default'
+	  }
+	}, {
+	  useMasterKey: true,
+	  success: function() {
+	    console.log("Notification sent!");
+	  },
+	  error: function(error) {
+	    console.log(error);
+	  }
+	});
+}
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+//-----------------------------------------------------------------------------
+// Voting
+//-----------------------------------------------------------------------------
+
 Parse.Cloud.afterSave('Vote', function(request) {
-
 	var voteWeight = request.object.get("weight");
-
 	var photoPointer = request.object.get("photo");
 
+	// Get photo object being voted on.
 	var PhotoClass = Parse.Object.extend("Photo");
 	var query = new Parse.Query(PhotoClass);
 	query.get(photoPointer.id, {
 	  success: function(photo) {
 	    var user = photo.get("createdBy");
-
 			var userId = user.id;
+			var voteCreatorPointer = request.object.get("createdBy");
 
-			var voteCreator = request.object.get("createdBy");
-
-			if (voteWeight == 1 && voteCreator.id != userId) {
-				var targetUser = new Parse.User();
-				targetUser.id = userId;
-
-				var query = new Parse.Query(Parse.Installation);
-				query.equalTo('user', user);
-
-				Parse.Push.send({
-				  where: query,
-				  data: {
-				    alert: 'You got an upvote on your photo!',
-				    badge: 'Increment',
-				    sound: 'default'
-				  }
-				}, {
-				  useMasterKey: true,
-				  success: function() {
-				    console.log("Notification for vote sent!");
-				  },
-				  error: function(error) {
-				    console.log(error);
+			if (voteWeight == 1 && voteCreatorPointer.id != userId) {
+				// Get user who created vote.
+				var UserClass = Parse.Object.extend("User");
+				var voteCreatorQuery = new Parse.Query(UserClass);
+				voteCreatorQuery.get(voteCreatorPointer.id, {
+					success: function(voteCreator) {
+						var pushNotificationMessage = String(voteCreator.get("username")).capitalizeFirstLetter()  + " upvoted your photo!"
+						sendPushNotificationMessageWithUser('You got an upvote on your photo!', user)
+					},
+				  error: function(object, error) {
+				  	sendPushNotificationMessageWithUser('You got an upvote on your photo!', user)
 				  }
 				});
 			}
